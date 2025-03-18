f8e6fd6126c6a0a4968a755e1cdc8695a8f85236
chkvdm <vadikvr6@outlook.com>
2025-03-18 15:45:06 +0400

    modify: add new action for modal buttons

--- src/features/race/components/RaceResultModal/RaceResultModal.tsx
+++ src/features/race/components/RaceResultModal/RaceResultModal.tsx
@@ -3,28 +3,27 @@ import { Modal } from 'antd';
 import { useAppDispatch, useAppSelector } from '@reduxHook';
 import { RootState } from '@store';
 import useRaceCarService from '@raceFeatures/services/raceCarServices';
-import { stopRace } from '@raceFeatures/slices/raceThunks';
+import { setIsWinnerModalOpen } from '@raceFeatures/slices/raceSlice';
 import RaceStatus from '@raceTypes/enums/race.enums';
 
 const RaceModal: React.FC = () => {
-  const { status, winner } = useAppSelector((state: RootState) => state.race);
-  const { cars } = useAppSelector((state: RootState) => state.garage);
-  const { stopCar, winnerRecord } = useRaceCarService();
+  const { status, winner, isWinnerModalOpen } = useAppSelector((state: RootState) => state.race);
+  const { winnerRecord } = useRaceCarService();
   const dispatch = useAppDispatch();
 
   const handleOk = async (): Promise<void> => {
     winnerRecord();
-    dispatch(stopRace({ cars, stopCar }));
+    dispatch(setIsWinnerModalOpen(false));
   };
 
   const handleCancel = async (): Promise<void> => {
-    dispatch(stopRace({ cars, stopCar }));
+    dispatch(setIsWinnerModalOpen(false));
   };
 
   return (
     <Modal
       title={`${winner.name} Win!`}
-      open={status === RaceStatus.FINISHED && !!winner.name}
+      open={status === RaceStatus.FINISHED && !!winner.name && isWinnerModalOpen}
       closable={false}
       onOk={handleOk}
       okText="Save result"
