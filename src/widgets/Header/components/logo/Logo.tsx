f9566936952739ea8733305789c08f789aa74a21
chkvdm <vadikvr6@outlook.com>
2025-03-18 04:02:19 +0400

    docs: update logo style path

--- src/widgets/Header/components/logo/Logo.tsx
+++ src/widgets/Header/components/logo/Logo.tsx
@@ -1,4 +1,4 @@
-import styles from 'widgets/Header/components/Logo/logo.module.scss';
+import styles from 'widgets/Header/components/logo/logo.module.scss';
 
 const Logo = (): JSX.Element => {
   return <img className={styles.headerLogo} src="/header-logo.webp" alt="header-logo" />;
